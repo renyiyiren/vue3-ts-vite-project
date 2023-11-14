8dde4280249488add778f8d01feb0f64beb2e540
renyiyiren <3325644826@qq.com>
2023-11-14 16:17:49 +0800

    分离pinia

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { createApp } from "vue";
 import Antd from "ant-design-vue";
 import App from "./App.vue";
-import { createPinia } from "pinia";
 import { setUpdateRouter } from "./router";
 import "ant-design-vue/dist/reset.css";
+import { setupStore } from "./store";
 const app = createApp(App);
-const pinia = createPinia();
 
 setUpdateRouter(app);
+setupStore(app);
 app.use(Antd);
-app.use(pinia);
+
 app.mount("#app");
